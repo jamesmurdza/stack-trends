269d34702bfc887034c090748e16b41435f0714d
James Murdza <james@jamesmurdza.com>
2023-02-08 15:11:56 +0000

    Fix bad indentation.

--- compare.js
+++ compare.js
@@ -14,44 +14,41 @@ let results = [];
 
 for (const tool of tools) {
     let path = `tools/${tool}.json`;
-        let rawdata = fs.readFileSync(path);
-        let timeline = JSON.parse(rawdata);
-        const sortDate = (a, b) => b < a ? -1 : (a > b ? 1 : 0);
-        const parseCount = count => parseFloat(count) * ((count || "").includes("K") ? 1000 : 1);
-        timeline.sort((a, b) => sortDate(b.time, a.time));
-        let times = timeline.map(({ time }) => (new Date(time)).getTime());
-        let stacks = timeline.map(({ stacks }) => parseCount(stacks));
+    let rawdata = fs.readFileSync(path);
+    let timeline = JSON.parse(rawdata);
+    const sortDate = (a, b) => b < a ? -1 : (a > b ? 1 : 0);
+    const parseCount = count => parseFloat(count) * ((count || "").includes("K") ? 1000 : 1);
+    timeline.sort((a, b) => sortDate(b.time, a.time));
+    let times = timeline.map(({ time }) => (new Date(time)).getTime());
+    let stacks = timeline.map(({ stacks }) => parseCount(stacks));
 
-        let dateB = (new Date("1 Jan 2023")).getTime();
-        let dateA = (new Date("1 Jan 2022")).getTime();
+    let dateB = (new Date("1 Jan 2023")).getTime();
+    let dateA = (new Date("1 Jan 2022")).getTime();
 
-        let ms = times.map(time => (new Date(time)).getTime());
+    let ms = times.map(time => (new Date(time)).getTime());
 
-        let bestFitFunction;
-        try {
-            bestFitFunction = functionToFit(ms, stacks, 2);
-        } catch (error) {
-            bestFitFunction = x => null;
-        }
-        const projection = x => Math.max(bestFitFunction(x), 0);
+    let bestFitFunction;
+    try {
+        bestFitFunction = functionToFit(ms, stacks, 2);
+    } catch (error) {
+        bestFitFunction = x => null;
+    }
+    const projection = x => Math.max(bestFitFunction(x), 0);
 
-        let increase = projection(dateB) / projection(dateA);
-        if (stacks[stacks.length-1] > 200)
-            results.push({
+    let increase = projection(dateB) / projection(dateA);
+    if (stacks[stacks.length - 1] > 200)
+        results.push({
             'name': tool,
-            'growth': Math.round(increase * 100 - 100) || 0 ,
-            'a': Math.round(projection(dateA)),
-            'b': Math.round(projection(dateB)),
-            });
-    }
+            'growth': Math.round(increase * 100 - 100) || 0,
+            'year1': Math.round(projection(dateA)),
+            'year2': Math.round(projection(dateB)),
+        });
+}
 
-    const sortResult = (a, b) => b < a ? -1 : (a > b ? 1 : 0);
-    results.sort((a, b) => sortResult(a.growth, b.growth));
-    for (let result of results) {
-        console.log( result.growth + "% " + result.a + ' ' + result.b + ' ' + result.name)
-    }
+const sortResult = (a, b) => b < a ? -1 : (a > b ? 1 : 0);
+results.sort((a, b) => sortResult(a.growth, b.growth));
 
-
-
-
+for (let result of results) {
+    //console.log(result.growth + "% " + result.year1 + ' ' + result.year2 + ' ' + result.name)
+}
 
